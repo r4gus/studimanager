33ff02ed4ef6dbf41b97ff2301f6b65f6ea2c80d
lukas <lukasmendel14043@gmail.com>
2019-07-29 14:00:23 +0200

    It was continued to create the Gui design of the main window.

---
+++ src/guiKalender/ControllerKalender.java
@@ -0,0 +1,15 @@
+package guiKalender;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerKalender implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        int a = 1;
+        int b= 2;
+        a = 3;
+    }
+}
