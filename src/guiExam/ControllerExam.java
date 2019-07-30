da217d7f5559bff3ad287a84b73cf0401f643821
Lukas Mendel <lukasmendel14043@gmail.com>
2019-07-30 09:37:15 +0200

    start creating Gui-Exam and controller logic

--- src/guiExam/ControllerExam.java
+++ src/guiExam/ControllerExam.java
@@ -1,11 +1,20 @@
 package guiExam;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerExam implements Initializable {
+
+    @FXML
+    private TextField textFieldLectureNumber;
+
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
