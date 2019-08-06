e21394fabf32595bfa5f7e49dddf857612a528b6
Lukas Mendel <lukasmendel14043@gmail.com>
2019-08-06 19:13:14 +0200

    It is now possible to dynamically add list elements to the Gui.

--- src/guiTodolist/ControllerTodolist.java
+++ src/guiTodolist/ControllerTodolist.java
@@ -3,7 +3,9 @@ package guiTodolist;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
@@ -46,14 +48,24 @@ public class ControllerTodolist implements Initializable {
     public void createNewSection()
     {
         VBox vBoxList = new VBox(10);
-        vBoxList.setPrefWidth(50);
+        vBoxList.setPrefWidth(200);
         vBoxList.setPrefWidth(200);
         hboxToDoLists.setMargin( vBoxList , new Insets(10,10,10,10));
 
+        /*  add List controls and heading */
+        HBox hBoxHeading = new HBox();
+        Label labelHeading = new Label("Überschrift");
+        labelHeading.setPadding(new Insets(2,10,2,10));
+        Button button = new Button("...");
 
+        hBoxHeading.getChildren().addAll(labelHeading, button);
+        vBoxList.getChildren().add(hBoxHeading);
+        hBoxHeading.setAlignment(Pos.CENTER_RIGHT);
+        hBoxHeading.setMargin( labelHeading , new Insets(0,35,0,0));
 
         vBoxList.getStyleClass().add("vBox");
-        hboxToDoLists.getChildren().addAll(vBoxList );
+        vBoxList.setMargin( hBoxHeading , new Insets(10,10,10,10));
+        hboxToDoLists.getChildren().add(vBoxList);
 
     }
 
